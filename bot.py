012a5055128323e8c96c3f48fdcf92bb2f5446cd
jas-writes-code <virgil@virgildoesthings.com>
2025-09-30 19:11:02 +0200

    quick update

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
+from discord.ext import tasks
 from discord import *
 import hellmonitor, wrangler
 import json
+import asyncio
 
 client = Client(intents=Intents.all())
 with open("key.json", "r") as file:
-    token = json.load(file)
-key = token["key"]
+    info = json.load(file)
+key = info["key"]
 
-async def major_order(message):
-    async with message.channel.typing():
+@tasks.loop(minutes=1)
+async def monitor():
+    target = client.get_channel(int(info["news"]))
+    discard, state = await hellmonitor.fetch("/api/v1/assignments")
+    if state % 28 == 0:
+        await major_order(target)
+    discard, state = await hellmonitor.fetch("/api/v1/dispatches")
+    if state % 28 == 0:
+        await dispatch(target)
+
+async def major_order(channel):
+    async with channel.typing():
         content = ""
         mo, mostate = await hellmonitor.fetch("/api/v1/assignments")
         briefing, brstate = await hellmonitor.fetch("/raw/api/WarSeason/801/Status")
         if mo == "Error":
-            message.channel.send(f"An error has occurred: {mostate}")
+            channel.send(f"An error has occurred: {mostate}")
             return
         briefing = briefing["globalEvents"]
         for event in briefing:
@@ -27,14 +39,14 @@ async def major_order(message):
             content += f"\n\n**MAJOR ORDER:**\n\n{mo_content}"
             content += f'\n\n*Expires* <t:{int(await wrangler.retime(event["expiration"]))}:R>'
 
-    await message.reply(content)
+    await channel.send(content)
 
-async def dispatch(message):
-    async with message.channel.typing():
+async def dispatch(channel):
+    async with channel.typing():
         content = ""
         dis, distate = await hellmonitor.fetch("/api/v1/dispatches")
         if dis == "Error":
-            message.channel.send(f"An error has occurred: {distate}")
+            channel.send(f"An error has occurred: {distate}")
             return
         if distate % 28 == 0:
             content += "**NEW DISPATCHES:**"
@@ -46,7 +58,7 @@ async def dispatch(message):
                 content += " **UNREAD**"
             content += f'\n{await wrangler.sanitize(element["message"])}'
 
-    await message.reply(content)
+    await channel.send(content)
 
 @client.event
 async def on_ready():
@@ -55,6 +67,10 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----')
+    if info["monitor"]:
+        print("\033[91mWARNING: Automated monitoring is enabled.\033[0m")
+        print("If you don't want this, stop the bot and set the option in key.json to false.")
+        monitor.start()
     print("The bot works -- this terminal will not provide further feedback unless there is an error. It's safe to detatch this window.")
 
 @client.event
@@ -69,8 +85,10 @@ async def on_message(message):
             action_name = commands[cmd]["action"]
             func = globals().get(action_name)  # look up function by name
             if callable(func):
-                await func(message)
+                await func(message.channel)
             else:
                 print(f"No function defined for action '{action_name}'")
+
+
 
 client.run(key)
